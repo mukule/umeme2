1bf993f3ff6b926bea4849f6cc8c9f50e85a3060
mukule <nelsonmasibo6@gmail.com>
2024-08-09 11:02:24 +0300

    db errors

--- ejobs/urls.py
+++ ejobs/urls.py
@@ -2,10 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import handler404, handler500
-
-handler404 = 'error_handling.views.custom_404'
-handler500 = 'error_handling.views.custom_500'
 
 
 urlpatterns = [
